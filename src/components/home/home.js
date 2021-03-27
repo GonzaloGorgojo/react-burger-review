645657e440770160fbbb2330a392bec030e4b956
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-03-27 14:02:34 -0300

    update home and readme

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,7 +8,9 @@ export function Home() {
           <h1>Welcome to the Burger Reviews Project</h1>
           <hr></hr>
           <h5>
-            Here you will find Peoples reviews about their favorite Burgers !
+            Here you will find Peoples reviews about their favorite Burgers, and
+            you can send your own review so we can share it with the world,
+            enjoy !
           </h5>
         </Col>
       </Row>
